940dd600688c54ba1c0af98e2979aaa90f51c6b2
Krzysztof Nalepa <kraleppa@gmail.com>
2023-10-30 19:36:20 +0100

    Index view styling

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -3,12 +3,37 @@
 @section('content')
     
     @foreach ($events as $event)
-        <div>{{ $event->name }}</div>
+    <div class="row">
+        <!-- timeline item 1 left dot -->
+        <div class="col-auto text-center flex-column d-none d-sm-flex">
+            <div class="row h-50">
+                @if (!$loop->first)
+                    <div class="col border-end">&nbsp;</div>
+                    <div class="col">&nbsp;</div>
+                @endif
+            </div>
+            <h5 class="m-2">
+                <span class="badge rounded-pill bg-light border">&nbsp;</span>
+            </h5>
+            <div class="row h-50">
+                @if (!$loop->last)
+                    <div class="col border-end">&nbsp;</div>
+                    <div class="col">&nbsp;</div>
+                @endif
+            </div>
+        </div>
+        <!-- timeline item 1 event content -->
+        <div class="col py-2">
+            <div class="card">
+                <div class="card-body">
+                    <div class="float-end text-muted">From {{ $event->start_date }} to {{ $event->end_date }}</div>
+                    <h4 class="card-title">{{ $event->name }}</h4>
+                    <p>{{ $event->description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        
     @endforeach 
-
-    <button type="button" class="btn btn-primary">Primary</button>
-
-
-
 
 @endsection
