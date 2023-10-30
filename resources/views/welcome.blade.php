5cd2b3d6a7a0a51e62be31616523b6468b3fc614
Krzysztof Nalepa <kraleppa@gmail.com>
2023-10-30 18:39:33 +0100

    Better styling of welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,12 @@
     </h1>
 
     <div class="d-flex align-items-center justify-content-between p-4">
-        <div class="d-flex flex-column gap-4 align-items-center">
+        <div class="d-flex flex-column gap-4">
             <div>            
                 <h4>We love helping you thrive!</h4>
                 Simplify Your Life. Sign up now and master your schedule!
             </div>
-            <a href="/events" class="btn btn-success w-75">Manage your events</a>
+            <a href="/events" class="btn btn-primary w-75">Manage your events</a>
 
         </div>
 
