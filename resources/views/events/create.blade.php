3983237aa20f8ffab6f5a9e3696204716ce4cd99
Krzysztof Nalepa <kraleppa@gmail.com>
2023-11-02 14:58:54 +0100

    Add event form styling

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,19 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-    <form action="/events" method="POST">
+    <form action="/events" method="POST" class="w-75">
         @csrf
-        <input type="text" name="name">
-        <input type="text" name="description">
-        <input type="date" name="start_date">
-        <input type="date" name="end_date">
-        <input type="text" name="image_url">
-        <select class="form-select" aria-label="Category" name="category_id">
-            <option selected>Open this select menu</option>
-            @foreach ($categories as $category)
-                <option value={{ $category->id }}>{{ $category->name }}</option>
-            @endforeach
-        </select>
-        <button type="submit">Add</button> 
+        <div class="mt-3">
+            <label for="name-input" class="form-label">Name</label>
+            <input id="name-input"  class="form-control" type="text" name="name">
+        </div>
+
+        <div class="mt-3">
+            <label for="category-input" class="form-label">Category</label>
+            <select id="category-input" class="form-select" aria-label="Category" name="category_id">
+                <option selected>Open this select menu</option>
+                @foreach ($categories as $category)
+                    <option value={{ $category->id }}>{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mt-3">
+            <label for="description-input" class="form-label">Description</label>
+            <textarea id="description-input" class="form-control" rows="3" name="description"></textarea>
+        </div>
+
+        <div class="mt-3">
+            <label for="start-date-input" class="form-label">Start date</label>
+            <input id="start-date-input"  class="form-control" type="date" name="start_date">
+        </div>
+
+        <div class="mt-3">
+            <label for="end-date-input" class="form-label">End date</label>
+            <input id="end-date-input"  class="form-control" type="date" name="end_date">
+        </div>
+
+        <div class="mt-3">
+            <label for="image-url-input" class="form-label">Image url</label>
+            <input id="image-url-input"  class="form-control" type="text" name="image_url">
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-4">Add</button> 
     </form>
 @endsection
