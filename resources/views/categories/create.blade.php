c5087cc0081b9b31dbce0ba78853cf2c0d2b3c7d
Krzysztof Nalepa <kraleppa@gmail.com>
2023-11-05 22:49:47 +0100

    Added validation on categories

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -4,13 +4,13 @@
     <form action="/categories" method="POST" class="w-75">
         @csrf
         <div class="mt-3">
-            <label for="name-input" class="form-label">Name</label>
-            <input id="name-input"  class="form-control" type="text" name="name">
+            <label for="name-input" class="form-label">Name <span class="text-danger">*</span></label>
+            <input id="name-input"  class="form-control" type="text" name="name" required>
         </div>
 
         <div class="mt-3 w-25">
-            <label for="color-input" class="form-label">Color</label>
-            <input id="color-input"  class="form-control" type="color" name="color">
+            <label for="color-input" class="form-label">Color <span class="text-danger">*</span></label>
+            <input id="color-input"  class="form-control" type="color" name="color" required>
         </div>
 
         <button type="submit" class="btn btn-primary mt-4">Add</button> 
