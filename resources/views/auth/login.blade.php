8e42f1c1151a21bd56c45a550a790e9d183479b9
Krzysztof Nalepa <kraleppa@gmail.com>
2023-11-05 23:23:14 +0100

    Added error display to auth forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,29 @@
 @extends('layouts.master')
 
 @section('content')
+    {{-- {{ var_dump() }} --}}
     <div class="d-flex flex-column align-items-center mt-5">
         <form method="POST" action="/login" class="w-50">
             @csrf
             <div class="form-outline mb-4">
-                <label class="form-label" for="email">Email address</label>
+                <label class="form-label" for="email">Email address <span class="text-danger">*</span></label>
                 <input id="email" class="form-control" type="email" name="email" required/>
-            </div>
+                @if ($errors->get('email'))
+                    @foreach ((array) $errors->get('email') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
         
+            </div>
+
             <div class="form-outline mb-4">
-                <label class="form-label" for="password">Password</label>
+                <label class="form-label" for="password">Password <span class="text-danger">*</span></label>
                 <input id="password" class="form-control" type="password" name="password" required />
+                @if ($errors->get('password'))
+                    @foreach ((array) $errors->get('password') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
             </div>
         
             <div class="row mb-4">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,23 +5,43 @@
         <form method="POST" action="/register" class="w-50">
             @csrf
             <div class="form-outline mb-4">
-                <label class="form-label" for="name">Name</label>
+                <label class="form-label" for="name">Name <span class="text-danger">*</span></label>
                 <input id="name" class="form-control" type="name" name="name" required/>
+                @if ($errors->get('name'))
+                    @foreach ((array) $errors->get('name') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
             </div>
 
             <div class="form-outline mb-4">
-                <label class="form-label" for="email">Email address</label>
+                <label class="form-label" for="email">Email address <span class="text-danger">*</span></label>
                 <input id="email" class="form-control" type="email" name="email" required/>
+                @if ($errors->get('email'))
+                    @foreach ((array) $errors->get('email') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
             </div>
         
             <div class="form-outline mb-4">
-                <label class="form-label" for="password">Password</label>
+                <label class="form-label" for="password">Password <span class="text-danger">*</span></label>
                 <input id="password" class="form-control" type="password" name="password" required />
+                @if ($errors->get('password'))
+                    @foreach ((array) $errors->get('password') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
             </div>
 
             <div class="form-outline mb-4">
-                <label class="form-label" for="password_confirmation">Confirm Password</label>
+                <label class="form-label" for="password_confirmation">Confirm Password <span class="text-danger">*</span></label>
                 <input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required />
+                @if ($errors->get('password_confirmation'))
+                    @foreach ((array) $errors->get('password_confirmation') as $message)
+                        <div class="text-danger">{{ $message }}</div>
+                    @endforeach
+                @endif
             </div>
         
             <button type="submit" class="btn btn-primary btn-block mb-4 w-100">Sign in</button>
