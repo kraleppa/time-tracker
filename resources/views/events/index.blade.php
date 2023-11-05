f92d2ecafb95793594a2f25d5012c854c53ccfce
Krzysztof Nalepa <kraleppa@gmail.com>
2023-11-05 23:46:47 +0100

    Event printing support

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,43 +1,51 @@
 @extends('layouts.master')
 
 @section('content')
-    
-    @foreach ($events as $event)
-        <a href="/events/{{ $event->id }}" class="row nostyle">
-            <div class="col-auto text-center flex-column d-none d-sm-flex">
-                <div class="row h-50">
-                    @if (!$loop->first)
-                        <div class="col border-end">&nbsp;</div>
-                        <div class="col">&nbsp;</div>
-                    @endif
-                </div>
-                <h5 class="m-2">
-                    <span class="badge rounded-pill bg-light border">&nbsp;</span>
-                </h5>
-                <div class="row h-50">
-                    @if (!$loop->last)
-                        <div class="col border-end">&nbsp;</div>
-                        <div class="col">&nbsp;</div>
-                    @endif
-                </div>
-            </div>
-            <div class="col py-2">
-                <div class="card w-50">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $event->name }}</h4>
-                        <div class="mt-2">{{ date_format(date_create($event->start_date), 'j F Y') }} - {{ date_format(date_create($event->end_date), 'j F Y') }}</div>
-                        @if ($event->category !== null )
-                        <div class="d-flex align-items-center gap-2 mt-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="{{ $event->category->color }}" class="bi bi-circle-fill" viewBox="0 0 16 16">
-                                <circle cx="8" cy="8" r="8"/>
-                            </svg>
-                            <span>{{ $event->category->name }}</span>
+    <div class="row">
+        <div class="col">
+            @foreach ($events as $event)
+                <a href="/events/{{ $event->id }}" class="row nostyle">
+                    <div class="col-auto text-center flex-column d-none d-sm-flex">
+                        <div class="row h-50">
+                            @if (!$loop->first)
+                                <div class="col border-end">&nbsp;</div>
+                                <div class="col">&nbsp;</div>
+                            @endif
                         </div>
-                    @endif
+                        <h5 class="m-2">
+                            <span class="badge rounded-pill bg-light border">&nbsp;</span>
+                        </h5>
+                        <div class="row h-50">
+                            @if (!$loop->last)
+                                <div class="col border-end">&nbsp;</div>
+                                <div class="col">&nbsp;</div>
+                            @endif
+                        </div>
                     </div>
-                </div>
-            </div>
-        </a>
-    @endforeach 
+                    <div class="col py-2">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $event->name }}</h4>
+                                <div class="mt-2">{{ date_format(date_create($event->start_date), 'j F Y') }} - {{ date_format(date_create($event->end_date), 'j F Y') }}</div>
+                                @if ($event->category !== null )
+                                <div class="d-flex align-items-center gap-2 mt-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="{{ $event->category->color }}" class="bi bi-circle-fill" viewBox="0 0 16 16">
+                                        <circle cx="8" cy="8" r="8"/>
+                                    </svg>
+                                    <span>{{ $event->category->name }}</span>
+                                </div>
+                            @endif
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            @endforeach 
+        </div>
 
+        <div class="col text-end" id="events-print-panel">
+            <button onclick="window.print();" class="mb-5 my-2 btn btn-secondary">
+                Print events
+            </button>
+        </div>
+    </div>
 @endsection
